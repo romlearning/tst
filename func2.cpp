824099f4d07286bff25d9808dc206c6e7b2eb23d
Rom <rom@home>
2015-06-21 19:05:54 +0300

    func2.cpp and func2.hpp were added

---
+++ func2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void func2() {
+	cout << "func2() call" << endl;
+}
---
+++ func2.hpp
@@ -0,0 +1,6 @@
+#ifndef FUNC2
+#define FUNC2
+
+void func2();
+
+#endif
